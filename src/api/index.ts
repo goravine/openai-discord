97ab840b2b8bc89fb7daceb2465d917d0fd22c1c
goravine <82170185+goravine@users.noreply.github.com>
2023-06-14 12:37:46 +0700

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,12 +5,13 @@ import {
   CreateImageRequestSizeEnum,
   ImagesResponse,
   OpenAIApi,
-} from 'openai';
-import process from 'process';
-import { AI } from '@/models/ai';
-import { Runnable } from '@/models/runnable';
-import { Logger } from '@/logger';
-import axios, { AxiosError } from 'axios';
+} from "openai";
+import process from "process";
+import { AI } from "@/models/ai";
+import { Runnable } from "@/models/runnable";
+import { Logger } from "@/logger";
+import axios, { AxiosError } from "axios";
+const { v4: uuidv4 } = require("uuid");
 
 export class Api implements AI, Runnable {
   /**
@@ -41,8 +42,8 @@ export class Api implements AI, Runnable {
      * Create OpenAI API configuration with API key
      */
     this._configuration = new Configuration({
-      organization: process.env.OPENAI_ORGANIZATION_ID + '',
-      apiKey: process.env.OPENAI_API_KEY + '',
+      organization: process.env.OPENAI_ORGANIZATION_ID + "",
+      apiKey: process.env.OPENAI_API_KEY + "",
     });
   }
 
@@ -52,7 +53,9 @@ export class Api implements AI, Runnable {
   run(): void {
     try {
       this._api = new OpenAIApi(this._configuration); // Create API instance
-      this._logger.logService.info('OpenAI Service has been initialized successfully.'); // Log service initialization
+      this._logger.logService.info(
+        "OpenAI Service has been initialized successfully."
+      ); // Log service initialization
     } catch (error) {
       this._logger.logService.error(`Failed to start OpenAI Service: ${error}`); // Log service initialization error
       process.exit(1); // Exit process
@@ -64,32 +67,43 @@ export class Api implements AI, Runnable {
    * @param chatHistory - Chat history to generate completion from
    * @returns {ChatCompletionResponseMessage} - Chat completion response object containing the completion
    */
-  public conversationId = '';
+  public conversationId = "";
 
-  async chatCompletion(chatHistory: ChatCompletionRequestMessage[]): Promise<ChatCompletionResponseMessage> {
+  async chatCompletion(
+    chatHistory: ChatCompletionRequestMessage[]
+  ): Promise<ChatCompletionResponseMessage> {
+    if (this.conversationId == "") {
+      this.conversationId = uuidv4();
+    }
+
     try {
-      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-        model: process.env.MODEL_NAME,
-        messages: chatHistory,
-        max_tokens: 1024, // Adjust the maximum number of tokens per request
-        temperature: 0.5, // Adjust the temperature for response generation
-        frequency_penalty: 0.6, // Adjust the frequency penalty for response generation
-        presence_penalty: 0.4, // Adjust the presence penalty for response generation
-        context: this.conversationId // Include the conversation ID in the request
-      }, {
-        headers: {
-          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-          'Content-Type': 'application/json'
+      const response = await axios.post(
+        "https://api.openai.com/v1/chat/completions",
+        {
+          model: process.env.MODEL_NAME,
+          messages: chatHistory,
+          max_tokens: 1024, // Adjust the maximum number of tokens per request
+          temperature: 0.5, // Adjust the temperature for response generation
+          frequency_penalty: 0.6, // Adjust the frequency penalty for response generation
+          presence_penalty: 0.4, // Adjust the presence penalty for response generation
+          context: this.conversationId, // Include the conversation ID in the request
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+            "Content-Type": "application/json",
+          },
         }
-      });
+      );
 
       // Update the conversation ID for subsequent requests
       this.conversationId = response.data.id;
-  
+
       return response.data.choices[0].message as ChatCompletionResponseMessage;
     } catch (error: any) {
-      this._logger.logService(`OpenAI API Headers: ${(error as AxiosError).headers}`);
-      this._logger.logService(`OpenAI API Message: ${(error as AxiosError).message}`);
+      this._logger.logService(
+        `OpenAI API Message: ${(error as AxiosError).message}`
+      );
       throw error;
     }
   }
@@ -101,21 +115,26 @@ export class Api implements AI, Runnable {
    * @param size - Size of the image (e.g. "512x512") (max "1024x1024")
    * @returns {ImagesResponse} - Images response object containing the image URLs
    */
-  async createImage(prompt: string, quantity: number, size: CreateImageRequestSizeEnum)
-    : Promise<ImagesResponse> {
+  async createImage(
+    prompt: string,
+    quantity: number,
+    size: CreateImageRequestSizeEnum
+  ): Promise<ImagesResponse> {
     /**
      * Create image request and return response or throw error
      */
-    const request = await this._api.createImage({
-      prompt,
-      n: quantity,
-      size,
-    }).then((response) => response.data)
+    const request = await this._api
+      .createImage({
+        prompt,
+        n: quantity,
+        size,
+      })
+      .then((response) => response.data)
       .catch((error: Error) => {
         this._logger.logService.error(`Failed to get image ${error.message}`); // Request failed
         throw error;
       });
 
-    return (request as ImagesResponse);
+    return request as ImagesResponse;
   }
 }
