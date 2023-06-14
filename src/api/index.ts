06881831e48cd7e9882ccd6fda712c71ed280ad0
goravine <82170185+goravine@users.noreply.github.com>
2023-06-14 11:10:07 +0700

    Update index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -74,7 +74,16 @@ export class Api implements AI, Runnable {
       temperature: 0.8, // Adjust the temperature for response generation
       frequency_penalty: 0.6, // Adjust the frequency penalty for response generation
       presence_penalty: 0.4, // Adjust the presence penalty for response generation  
-    }).then((response) => response.data.choices[0].message)
+    })
+      .then((response) => {
+        response.data.choices[0].message
+        // Print rate limit headers
+        console.log('Rate Limit Remaining:', response.headers['x-ratelimit-remaining']);
+        console.log('Rate Limit Reset:', response.headers['x-ratelimit-reset']);
+        console.log('Rate Limit Limit:', response.headers['x-ratelimit-limit']);
+    
+        return response.data.choices[0].message;
+      })
       .catch((error: Error) => {
         this._logger.logService.error(`Failed to get chat completion: ${error.message}`); // Request failed
         throw error;
