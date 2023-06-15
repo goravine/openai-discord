c06cc36ddfb1264c2aa3cf84de7705d3365129cd
goravine <82170185+goravine@users.noreply.github.com>
2023-06-15 14:11:03 +0700

    Update index.ts

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -127,12 +127,7 @@ export class Bot implements Runnable {
       });
     });*/
     // Set presence using setPresence() method with null check
-    this._client.user.setPresence({
-      activity: {
-        name: 'Valorant',
-        type: 'PLAYING'
-      }
-    });
+    this._client.user?.setActivity('VALORANT', { type: ActivityType.Playing });
 
     /**
      *  On interaction create event handler
