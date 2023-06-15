0e552220e55941408c0705b01fa92119c2f78853
goravine <82170185+goravine@users.noreply.github.com>
2023-06-15 15:40:24 +0700

    Update index.ts

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,5 +1,5 @@
 import {
-  ActivityType, Client, CommandInteraction, IntentsBitField, Interaction, Partials, REST, Routes, voiceChannel,
+  ActivityType, Client, CommandInteraction, IntentsBitField, Interaction, Partials, REST, Routes, VoiceChannel,
 } from 'discord.js';
 import process from 'process';
 import { Logger } from '@/logger';
